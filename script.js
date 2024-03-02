4339b2565a7a838970070a64ca7bf285a6838fa0
urielsmp128 <urielsmp128@outlook.com>
2024-03-01 22:32:49 -0600

    Refactored JS code using functions

--- script.js
+++ script.js
@@ -26,34 +26,26 @@ function isValidEmail(email){
 
 }
 
+// Check required fields
+function checkRequired(inputArray){
+    inputArray.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is required`);
+        }else{
+            showSuccess(input);
+        }
+    });
+}
+
+// Get field name
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Evente Listeners
 form.addEventListener('submit', function(e){
     e.preventDefault();
     
-    if(username.value === ''){
-        showError(username, 'Username is required');
-    } else{
-        showSuccess(username)
-    }
-
-    if(email.value === ''){
-        showError(email, 'Email is required');
-    } else if (!isValidEmail(email.value)){
-        showError(email, 'Email is not valid')
-    }else{
-        showSuccess(email)
-    }
-
-    if(password.value === ''){
-        showError(password, 'Password is required');
-    } else{
-        showSuccess(password)
-    }
-
-    if(password_Confirmation.value === ''){
-        showError(password_Confirmation, 'Password Confirmation is required');
-    } else{
-        showSuccess(password_Confirmation)
-    }
+    checkRequired([username, email, password, password_Confirmation])
 
 });
